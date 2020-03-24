c0aa0e74bc5567decc7a5cf49c4b522070494952
ansushina <nastya.sushina@yandex.ru>
2020-03-24 14:20:43 +0300

    add fork

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import socket
 from threading import Thread
 from config import Config
 from functions import handle
+import os
 
 if __name__ == '__main__':
     conf = Config()
@@ -11,7 +12,26 @@ if __name__ == '__main__':
     sock.bind((conf.host, conf.port))
     sock.listen(1)
 
+    threads = []
+
     for i in range(conf.thread_limit):
         th = Thread(target=handle, args=(sock, conf.document_root))
-        th.start()
+        threads.append(th)
+
+    for i in range(conf.cpu_limit): 
+        n = os.fork()
+        if n == 0: 
+            for j in range(i, conf.thread_limit, conf.cpu_limit): 
+                threads[j].start()
+            print("child")
+            exit(0)
+        if n > 0:
+            print(n)
+        if n < 0:
+            print("error")
+            exit(1) 
+
+    for i in range(conf.cpu_limit):
+        er = os.wait()
+        print(er)
 
